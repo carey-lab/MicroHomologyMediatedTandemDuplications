2e1c38b790ef80b8c49219ef2a0c4671ac76722e
Lucas Carey <lucas.carey@gmail.com>
2020-03-06 10:32:08 +0800

    a

--- utils/MTDs_join_counttsv_with_fasta.py
+++ utils/MTDs_join_counttsv_with_fasta.py
@@ -47,45 +47,88 @@ cnts_tsv_df = pandas.read_csv(args.countstsv, sep='\t', names=["chr", "s1", "e1"
 toc = time.time()
 print(f"... loaded. Adding Sequence data took {toc - tic:0.2f} seconds")
 
+## smaller df for debugging 
+#cnts_tsv_df = cnts_tsv_df.loc[ (cnts_tsv_df["chr"]=="I") ]
+cnts_tsv_df = cnts_tsv_df.loc[ (cnts_tsv_df["chr"]=="I") | (cnts_tsv_df["chr"]=="II")  | (cnts_tsv_df["chr"]=="III")  ] 
+#cnts_tsv_df = cnts_tsv_df.loc[ (cnts_tsv_df["s1"] < 1000000) | (cnts_tsv_df["NDups"] > 0) ] 
+cnts_tsv_df = cnts_tsv_df.reset_index(drop=True) # rebuild row numbers in the shrunken dataframe
+
 cnts_tsv_df["s01"] = cnts_tsv_df.s1 - 1
 cnts_tsv_df["e01"] = cnts_tsv_df.e1 - 1
 cnts_tsv_df["s02"] = cnts_tsv_df.s2 - 1
 cnts_tsv_df["e02"] = cnts_tsv_df.e2 - 1
 
-# Assign MHseq, MHlen, & GC
+# put the position columns into the order needed to make a .bed file  : s01 & e2
 cnts_tsv_df["MH_length"] = cnts_tsv_df.e1 - cnts_tsv_df.s1 + 1
-MH = ["" for x in range(len(cnts_tsv_df))]
-interMH = ["" for x in range(len(cnts_tsv_df))]
+cnts_tsv_df = cnts_tsv_df[['chr','s01','e2','MH_length','NDups','s1','s2','e1','e01','s02','e02']]
 
+
+
+
+# Assign MHseq, MHlen, & GC
 tic = time.time()
+
+# way way way way faster to not work with dataframes
+#   extract all info into lists, save into a list, and put the result into the df
+MHseq = ["" for x in range(len(cnts_tsv_df))]
+interMHseq = ["" for x in range(len(cnts_tsv_df))]
+
+c=cnts_tsv_df['chr'].to_list()
+s1=cnts_tsv_df['s1'].to_list()
+e1=cnts_tsv_df['e1'].to_list()
+s02=cnts_tsv_df['s02'].to_list()
+
 for I in range(len(cnts_tsv_df)):
-    MH[I] = fa.fetch( cnts_tsv_df.chr[I] , (cnts_tsv_df.s1[I].item() , cnts_tsv_df.e1[I].item()) )
-    interMH[I] = fa.fetch( cnts_tsv_df.chr[I] , (cnts_tsv_df.e1[I].item()+1 , cnts_tsv_df.s02[I].item()) )
+    MHseq[I] = fa.fetch( c[I] , (s1[I] , e1[I] ) )
+    interMHseq[I] = fa.fetch( c[I] , (e1[I]+1 , s02[I]) )
 
 toc = time.time()
-cnts_tsv_df["MH_Sequence"] = MH
-cnts_tsv_df["InterMH_Sequence"] = interMH
-print(f"... done. Extracting sequences took {(toc - tic)/60:0.2f} minutes. Adding GC & modulo 3")
+cnts_tsv_df["MH_Sequence"] = MHseq
+# cnts_tsv_df["InterMH_Sequence"] = interMHseq     # makes the dataframe huge, and we don't need it
+print(f"... done. Extracting sequences took {(toc - tic)/60:0.2f} minutes. Adding GC & modulo 3  ...")
+del s1,e1,s02,c,MHseq
+
 
 cnts_tsv_df["MH_Sequence_GC"] = cnts_tsv_df["MH_Sequence"].apply(GC)
-cnts_tsv_df["InterMH_Sequence_GC"] = cnts_tsv_df["InterMH_Sequence"].apply(GC)
-cnts_tsv_df["DuplicatedRegion_GC"] = cnts_tsv_df["MH_Sequence"].add(cnts_tsv_df["InterMH_Sequence"]).apply(GC)
-cnts_tsv_df["CompleteMHP_GC"] = cnts_tsv_df["MH_Sequence"].add( cnts_tsv_df["MH_Sequence"].add(cnts_tsv_df["InterMH_Sequence"]) ).apply(GC)
-cnts_tsv_df["Duplication_Creates_modulo3_insertion"] = (cnts_tsv_df["MH_Sequence"].apply(len)+cnts_tsv_df["InterMH_Sequence"].apply(len)) % 3==0
+cnts_tsv_df["InterMH_Sequence_GC"] = list(map(GC,interMHseq))
+cnts_tsv_df["DuplicatedRegion_GC"] = cnts_tsv_df["MH_Sequence"].add(interMHseq).apply(GC)
+cnts_tsv_df["CompleteMHP_GC"] = cnts_tsv_df["MH_Sequence"].add( cnts_tsv_df["MH_Sequence"].add(interMHseq) ).apply(GC)
+cnts_tsv_df["Duplication_Creates_modulo3_insertion"] = (list(map(len,interMHseq))+cnts_tsv_df["MH_Sequence"].apply(len)) % 3==0
+#cnts_tsv_df["InterMH_Sequence_GC"] = cnts_tsv_df["InterMH_Sequence"].apply(GC)
+#cnts_tsv_df["DuplicatedRegion_GC"] = cnts_tsv_df["MH_Sequence"].add(cnts_tsv_df["InterMH_Sequence"]).apply(GC)
+#cnts_tsv_df["CompleteMHP_GC"] = cnts_tsv_df["MH_Sequence"].add( cnts_tsv_df["MH_Sequence"].add(cnts_tsv_df["InterMH_Sequence"]) ).apply(GC)
+#cnts_tsv_df["Duplication_Creates_modulo3_insertion"] = (cnts_tsv_df["MH_Sequence"].apply(len)+cnts_tsv_df["InterMH_Sequence"].apply(len)) % 3==0
 
+# this  make files smaller 
+cnts_tsv_df["MH_Sequence_GC"] = cnts_tsv_df["MH_Sequence_GC"].astype('uint8')
+cnts_tsv_df["InterMH_Sequence_GC"] = cnts_tsv_df["InterMH_Sequence_GC"].astype('uint8')
+cnts_tsv_df["DuplicatedRegion_GC"] = cnts_tsv_df["DuplicatedRegion_GC"].astype('uint8')
+cnts_tsv_df["CompleteMHP_GC"] = cnts_tsv_df["CompleteMHP_GC"].astype('uint8')
+cnts_tsv_df["MH_length"] = cnts_tsv_df["MH_length"].astype('uint8')
+
+# other variables needed for predicting MTD frequency
+cnts_tsv_df['InterMH_Distance'] = list(map(len,interMHseq))
+cnts_tsv_df['InterMH_Distance'] = cnts_tsv_df['InterMH_Distance'].astype('uint16')
+
+# we only need to save the position columns needed to make a .bed file  : s01 & e2
+cnts_tsv_df = cnts_tsv_df.drop(columns=['s1', 'e1', 's2', 'e01', 's02', 'e02'])
+
+#give start and end column headers good names
+cnts_tsv_df = cnts_tsv_df.rename(columns={"s01": "MHP_start", "e2": "MHP_end"})
 
 # save
 tic = time.time()
-cnts_tsv_df.to_pickle( "/Users/lcarey/df.pickle" )
+cnts_tsv_df.to_pickle( "/Users/lcarey/Downloads/df.pickle" )
 toc = time.time()
 print(f"Saving as pickle took {(toc - tic)/60:0.2f} minutes.")
 
 tic = time.time()
-cnts_tsv_df.to_hdf( "/Users/lcarey/df.h5" , key='df', mode='w')
+cnts_tsv_df.to_hdf( "/Users/lcarey/Downloads/df.h5" , key='df', mode='w')
 toc = time.time()
 print(f"Saving as h5 took {(toc - tic)/60:0.2f} minutes.")
 
 tic = time.time()
-cnts_tsv_df.to_feather( "/Users/lcarey/df.feather" , key='df', mode='w')
+cnts_tsv_df.to_feather( "/Users/lcarey/Downloads/df.feather" )
 toc = time.time()
 print(f"Saving as feather took {(toc - tic)/60:0.2f} minutes.")
+
