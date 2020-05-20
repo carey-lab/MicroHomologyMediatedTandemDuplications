deafbaa6a9ce08bd47b2755abe0343e0415546a0
Lucas Carey <lucas.carey@gmail.com>
2020-05-20 16:20:34 +0800

    now includes insertions and deletions, not just soft-clipped

--- utils/extract_clipped_reads_from_bam.sh
+++ utils/extract_clipped_reads_from_bam.sh
@@ -4,9 +4,9 @@
 # 
 # example: 
 #  extract_clipped_reads_from_bam.sh test.cram  > test.bam
+#     grep  '[[:space:]|[:digit:]M][[:digit:]]S' 
 {
 samtools view -H $1
-samtools view $1 | cut -f 1-10  \
-	| grep  '[[:space:]|[:digit:]M][[:digit:]]S' \
-	| awk '{print NR "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t"  $9 "\t" $10 "\t" "*" }' 
+samtools view $1 | cut -f 2-10  | \
+	awk '{ if ($5 ~ /[SID]/) print NR "\t" $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t"  $8 "\t" $9 "\t" "*" } '
 } | samtools view -h -O BAM - -o - 
