27411021a96994972d6f3bec37a3f9304b84c6f2
lcarey <lucas.carey@gmail.com>
2020-11-28 22:27:10 -0500

    changed from multilevel indicies to regular columns

--- Revision__Scer_Hap_vs_Dip_MAlines/ScerHapDip__FractionInEssentialGenes.py
+++ Revision__Scer_Hap_vs_Dip_MAlines/ScerHapDip__FractionInEssentialGenes.py
@@ -3,19 +3,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats 
+import os
 
 
+DATADIR = os.path.dirname(__file__) # folder with this script; also contains the data files
+image_file_basename = DATADIR + '/Scer_HapVSDip__MetaData_Ploidy_and_MAlines___'
+data_file_name = DATADIR+'/Scer_HapVSDip__dup_sites_found____genes_with_essentiality.txt.xz'
+anno_file_name = DATADIR+'/Scer_HapVSDip__MetaData_Ploidy_and_MAlines.csv.xz'
 
-DATADIR = '/Users/lcarey/Downloads/ScerHapDip/'
-file_name = 'Scer_HapVSDip__dup_sites_found____genes_with_essentiality.txt.xz'
+#%% Load data
 # I	2707	7235	intergenic	0	0	I	3725	3743	SRR6997021	0	3732
 col_names = ['chr','ORFstart','ORFnd','type','gene','orf','chr2','MHPleftStart','MHPright','SRA','Nreads','MHPleftend']
-df = pd.read_csv(DATADIR+file_name,sep='\t',names=col_names)
+df = pd.read_csv(data_file_name,sep='\t',names=col_names)
 df['MHlen'] = df.MHPleftend - df.MHPleftStart
 df['Essential'] = df.type.str.match('Essential')
+df['Non_essen'] = df.type.str.match('Non-essent')
+df['Intergenic'] = df.type.str.match('intergenic')
 df['SizeDivByThree'] = (df.MHPright-df.MHPleftStart)%3==0 ; 
 df.drop(columns='chr2',inplace=True)
 df = df[df.chr!='Mito']
+
 ## ### ### #### ### ### ### ### 
 # remove Nreads==0 
 #  must check why these exist!
@@ -24,7 +31,7 @@ df.reset_index(inplace=True,drop=True)
 ## ### ### #### ### ### ### ### 
 
 
-A = pd.read_csv('Scer_HapVSDip__MetaData_Ploidy_and_MAlines.csv.xz')
+A = pd.read_csv(anno_file_name)
 A = A[['SRA','Ploidy']]
 
 df = df.merge(A,on='SRA')
@@ -42,59 +49,45 @@ df.head()
 #      xz -dkc Scer_HapVSDip__dup_sites_found____genes_with_essentiality.txt.xz | cut -f 1-9 |sort|uniq -c|sort -nk 1|wc -l
 #           993
 #
-cols_to_use = ['ORFstart','ORFnd','type','orf','gene','Essential']
+cols_to_use = ['ORFstart','ORFnd','type','orf','gene','Essential','Non_essen','Intergenic']
 cols_to_use += ['chr','MHPleftStart','MHPright','MHPleftend','MHlen','SizeDivByThree']
 
 hap = df[df.Ploidy == 'hap']
 hap = hap.groupby(cols_to_use).agg('count')
 hap = hap[hap.Nreads<2]
+hap.reset_index(drop=False,inplace=True)
 
 dip = df[df.Ploidy == 'dip']
 dip = dip.groupby(cols_to_use).agg('count')
 dip = dip[dip.Nreads<2]
-
-#g = g['Nreads'].agg(['count']).sort_values(('Nreads','count'),ascending=False)
-#g = g.agg(['Ploidy']).sort_values(('Nreads','count'),ascending=False)
-#plt.hist(g[('Nreads','count')])
-#g = g[g[('Nreads','count')]<2]
-#dip = g[g.index.get_level_values('Ploidy') == 'dip']
-#hap = g[g.index.get_level_values('Ploidy') == 'hap']
+dip.reset_index(drop=False,inplace=True)
 
 print(len(dip))
 print(len(hap))
 
 # %%
-hap_essential = hap.index.get_level_values('Essential')
-dip_essential = dip.index.get_level_values('Essential')
-hap_intergenic = hap.index.get_level_values('type').str.match('intergenic')
-dip_intergenic = dip.index.get_level_values('type').str.match('intergenic')
-hap_nonessen = hap.index.get_level_values('type').str.match('Non-essential')
-dip_nonessen = dip.index.get_level_values('type').str.match('Non-essential')
-
 print('')
-print(f"# Essential & diploid = {np.sum(dip_essential)}")
-print(f"# Essential & haploid = {np.sum(hap_essential)}")
+print(f"# Essential & diploid = {np.sum(dip.Essential)}")
+print(f"# Essential & haploid = {np.sum(hap.Essential)}")
 print('')
-print(f"# Non-Essential & diploid = {np.sum(dip_nonessen)}")
-print(f"# Non-Essential  & haploid = {np.sum(hap_nonessen)}")
+print(f"# Non-Essential & diploid = {np.sum(dip.Non_essen)}")
+print(f"# Non-Essential  & haploid = {np.sum(hap.Non_essen)}")
 print('')
-print(f"# intergenic & diploid = {np.sum(dip_intergenic)}")
-print(f"# intergenic & haploid = {np.sum(hap_intergenic)}")
+print(f"# intergenic & diploid = {np.sum(dip.Intergenic)}")
+print(f"# intergenic & haploid = {np.sum(hap.Intergenic)}")
 print('')
 print(f"total # dip = {len(dip)}")
 print(f"total # hap = {len(hap)}")
 print('')
 
 
-print(f"Dip mean Div3 Essential = {np.mean(dip[dip_essential].index.get_level_values('SizeDivByThree'))}")
-print(f"Hap mean Div3 Essential = {np.mean(hap[hap_essential].index.get_level_values('SizeDivByThree'))}")
-print(f"Dip mean Div3 Non-Essen = {np.mean(dip[dip_nonessen].index.get_level_values('SizeDivByThree'))}")
-print(f"Hap mean Div3 Non-Essen = {np.mean(hap[hap_nonessen].index.get_level_values('SizeDivByThree'))}")
-print(f"Dip mean Div3 Intergenic = {np.mean(dip[dip_intergenic].index.get_level_values('SizeDivByThree'))}")
-print(f"Hap mean Div3 Intergenic = {np.mean(hap[hap_intergenic].index.get_level_values('SizeDivByThree'))}")
-print('')
+print(f"Dip mean Div3 Essential = {np.mean(dip['SizeDivByThree'][dip.Essential])}")
+print(f"Hap mean Div3 Essential = {np.mean(hap['SizeDivByThree'][hap.Essential])}")
+print(f"Dip mean Div3 Non-Essen = {np.mean(hap['SizeDivByThree'][dip.Non_essen])}")
+print(f"Hap mean Div3 Non-Essen = {np.mean(hap['SizeDivByThree'][hap.Non_essen])}")
+print(f"Dip mean Div3 Intergenic = {np.mean(hap['SizeDivByThree'][dip.Intergenic])}")
+print(f"Hap mean Div3 Intergenic = {np.mean(hap['SizeDivByThree'][hap.Intergenic])}")
 
-# %%
 
 def func(pct, allvals):
     absolute = int(pct/100.*np.sum(allvals))
